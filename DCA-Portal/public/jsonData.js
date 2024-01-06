fa5bdba596774269cbf93bd97e59a4e1c0f2c681
Jozcefhs <jozcefhse@gmail.com>
2024-01-06 04:41:06 +0100

    kill

--- DCA-Portal/public/jsonData.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-import cred from './st_keys.json' assert {type: 'json'};
-
-console.log(cred);
--- DCA-Portal/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controllers/authController')
-
-router.post('/register', authController.register)
-router.post('/login', authController.login)
-router.post('/loginTeacher', authController.loginTeacher)
-
-module.exports = router;
